a6f1dc478228cac714759f44a1892c8130e7a825
Mikhail-Bunkov <bunko04@bk.ru>
2021-11-21 20:43:20 +1000

    Done

---
+++ src/main/java/ru/bunkov/secondlesson/Main.java
@@ -0,0 +1,50 @@
+package ru.bunkov.secondlesson;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+//        List<Integer> ar = new ArrayList<>(); //тестовые движения. Не хочется тесты писать.
+//        ar.add(4);
+//        ar.add(9);
+//        ar.add(8);
+//        ar.add(7);
+//        ar.add(6);
+//        System.out.println(ar);
+//        System.out.println(ar.toString());
+//        ar.add(5);
+//        ar.add(9);
+//        ar.add(8);
+//        ar.add(7);
+//        ar.add(6);
+//        ar.add(5);
+//        System.out.println(ar);
+//        ar.remove(4);
+//        System.out.println(ar);
+//        System.out.println(ar.size());
+//        System.out.println(ar.contains(9));
+//        System.out.println(ar.get(2));
+
+
+//        LinkedList<Integer> list = new LinkedListImpl<>(); //проводил небольшие тестовые движения.
+//        list.isEmpty();
+//        list.insertFirst(14);
+//        list.insertFirst(16);
+//        list.insertFirst(15);
+//        list.insertFirst(11);
+//        list.insertFirst(12);
+//        list.display();
+//        System.out.println(list.getFirst());
+//        System.out.println(list.getLast());
+//        list.remove(15);
+//        list.display();
+//        System.out.println(list.size());
+//        System.out.println(list.isEmpty());
+//        list.removeFirst();
+//        System.out.println(list.getFirst());
+//        list.insertLast(20);
+//        list.display();
+    }
+}
